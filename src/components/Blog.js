011e1e9774e3c0765089f790e3ea7867f15d7963
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-28 23:33:05 +0100

    modified this components

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,24 +1,29 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Blog = () => {
-    const url = 'https://bmi-server.herokuapp.com/api/'
-    const [blogTray, setBlogTray] = useState([])
-    useEffect(()=>{
-        axios.get(`${url}writeup`).then((res)=>{
-            if(res.data.status){
-                setBlogTray(res.data.resp)
-            }
-        })
-    })
+    const blogTray = useSelector(state=>state.BlogReducer.blogTray)
+    const navigate = useNavigate()
+
     return (
-        <div className='bg-match-bmi py-5 px-5'>
+        <div className='bg-bmi py-5 px-5 mt-2'>
             <div className='row' >
             {
                 blogTray.map((each, i)=>(
-                        <div className='col-md-6'>
+                        <div className='col-lg-6 mx-auto' key={i}>
                             <div className='container bg-bmi shadow-lg rounded-lg py-2 my-4 text-bmi-match' >
-                                <h1>{each.title}</h1>
+                                <div className='d-flex justify-content-between'>
+                                    <p className='font-weight-bold text-white'>Time</p>
+                                    <img src={require('../assets/bmi_logo.png')} alt="Basileia Ministries Int'l" width="50px"/>
+                                </div>
+                                    <h1>{each.title}</h1>
+                                <p className='h6 mb-5'>{each.content.slice(0,50)}{each.content.length > 50 && (<span>...</span>)}</p>
+                                {
+                                    each.content.length > 50
+                                    &&
+                                    <p className='btn text-bmi-match' onClick={()=>navigate('/posts/' + each.title)}>Continue Reading <i className='fa fa-arrow-right'></i></p>
+                                }
                             </div>
                         </div>
 
