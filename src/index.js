691284bc13ec43a41ab1cf63c0fd4fe22d08d7a8
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-28 23:30:11 +0100

    implemented reducer

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import allReducer from './reducers/allReducer';
+
+const store = createStore(allReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter >
+    <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
