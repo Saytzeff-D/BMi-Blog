53fe64bc6310e020dea025942c7d588463b52cf7
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-28 23:29:13 +0100

    design the footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-match-bmi fixed-bottom'>
+            <p className='text-center text-bmi py-2 h6'>
+                &copy; 2022 | Designed by Basileia Ministries Int'l Team
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
