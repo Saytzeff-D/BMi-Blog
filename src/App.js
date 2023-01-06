e075d3d89bcdbf4eafceb87a1a3309b8773c71f9
excellence-arch <oladipupomichael9@gmail.com>
2023-01-06 12:20:35 +0300

    changed the text to white

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ export default function App() {
       <Navbar />
       <div class="social-buttons position-fixed">
         <a href="#"><i class="shadow-lg text-bmi-match fa fa-whatsapp"></i></a>
-  <a href="https://web.facebook.com/basileiaminintl"><i class="shadow-lg text-bmi-match fa fa-facebook-f"></i></a>
-  <a href="https://t.me/basileiaminintl"><i class="shadow-lg text-bmi-match fa fa-telegram"></i></a>
-  <a href="https://www.instagram.com/basileiaminintl/"><i class="shadow-lg text-bmi-match fa fa-instagram"></i></a>
+        <a href="https://web.facebook.com/basileiaminintl"><i class="shadow-lg text-bmi-match fa fa-facebook-f"></i></a>
+        <a href="https://t.me/basileiaminintl"><i class="shadow-lg text-bmi-match fa fa-telegram"></i></a>
+        <a href="https://www.instagram.com/basileiaminintl/"><i class="shadow-lg text-bmi-match fa fa-instagram"></i></a>
       </div>
       <Header />
       <Routes>
